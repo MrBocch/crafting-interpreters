940bd6e25a42dae491433c78a04413d0c4250ae6
MrBocch <jorgealberto1436@gmail.com>
2025-02-04 14:25:13 -0600

    just turning file into string for now

--- tree-walker/src/tree-walker.cr
+++ tree-walker/src/tree-walker.cr
@@ -1,2 +1,27 @@
-# TODO: Write documentation for `Tree::Walker`
-puts "Hello world"
+def main
+  if ARGV.size > 1
+    puts "Usage: jlox [script]"
+    exit(64)
+  elsif ARGV.size == 1
+    runFile(ARGV[0])
+  else
+    puts "go REPL"
+  end
+end
+
+def runFile(path : String) : Nil
+
+  if !File.file?(path)
+    puts "#{path} does not exist"
+    exit(64)
+  end
+
+  file = File.read(path)
+  # run file
+end
+
+def runPrompt() : Nil
+
+end
+
+main
