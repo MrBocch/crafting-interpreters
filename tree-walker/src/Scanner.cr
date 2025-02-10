0226bd667532a781dc472dde6e0f8a2981fdf8df
MrBocch <jorgealberto1436@gmail.com>
2025-02-10 15:40:51 -0600

    lexer recognises the one character tokens

--- tree-walker/src/Scanner.cr
+++ tree-walker/src/Scanner.cr
@@ -1,34 +1,63 @@
 require "./Token.cr"
 
+alias TT = TokenType
+
 class Scanner
 
-  private getter start   : UInt32 = 0
-  private getter current : UInt32 = 0
-  private getter line    : UInt32 = 1
+  property start   : UInt32 = 0
+  property current : UInt32 = 0
+  property line    : Int32  = 1
+  # crystal can feel so backwards at times, is skill issue?
+  # property tokens : Array(Token) = []
+  property tokens = [] of Token
 
   def initialize(source : String) : Nil
     @source = source
   end
 
   def scan_tokens() : Array(Token)
-    tokens : Array(Token)
+    while !is_at_end?
+      start = current
+      scan_token()
+    end
+
+    tokens.push(Token.new(TT::EOF, "", line))
+    return tokens
+  end
+
+  private def scan_token() : Nil
     c : Char = advance()
+
     case c
-    when '(' then
-    when v2
+    when '(' then add_token(TT::LEFT_PAREN)
+    when ')' then add_token(TT::RIGHT_PAREN)
+    when '{' then add_token(TT::LEFT_BRACE)
+    when '}' then add_token(TT::RIGHT_BRACE)
+    when ',' then add_token(TT::COMMA)
+    when '.' then add_token(TT::DOT)
+    when '-' then add_token(TT::MINUS)
+    when '+' then add_token(TT::PLUS)
+    when ';' then add_token(TT::SEMICOLON)
+    when '*' then add_token(TT::STAR)
     else
-      # default
-    return a
+      # how do i call this thing ?
+      # TreeWalker.error("unexpected character")
+      puts "Error, #{line}"
+      exit(64)
+    end
   end
 
-  private def addToken(type : TokenType) : Nil
-
+  private def add_token(type : TokenType) : Nil
+    # dont know what the object literal stuff is about
+    text = @source[start...current]
+    tokens.push(Token.new(type, text, line))
   end
+
 
   private def advance() : Char
     # not check if aoutof bounds?
     c : Char = @source[current]
-    current += 1
+    @current += 1
     # i never liked x++ , ++x, increments
     # but its so unclear, sometimes
     return c
@@ -46,5 +75,5 @@ class Scanner
   end
 end
 
-s = Scanner.new("byte stream?")
+s = Scanner.new("(-a")
 p! s.scan_tokens()
